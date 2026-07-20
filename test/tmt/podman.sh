020403a1f59beb79bc48f4694b12340c937c46d3
Jan Kaluza <jkaluza@redhat.com>
2026-07-20 08:35:21 +0200

    Run with RUST_BACKTRACE=1.
    Signed-off-by: Jan Kaluza <jkaluza@redhat.com>

--- test/tmt/podman.sh
+++ test/tmt/podman.sh
@@ -11,13 +11,17 @@ dnf install -y podman-tests bats conmon-v3
 rpm -q conmon-v3 podman containers-common-extra crun runc || true
 
 # Verify conmon-v3 is installed and is version 3.x
-/usr/bin/conmon-v3 --version | grep -qE '^conmon version 3(\.|$)'
+version=$(/usr/bin/conmon-v3 --version)
+echo "$version"
+grep -qE '^conmon version 3(\.|$)' <<< "$version"
 
 # Create symlink so Podman uses conmon-v3
 ln -sf /usr/bin/conmon-v3 /usr/bin/conmon
 
 # Verify the right conmon is being used
-/usr/bin/conmon --version | grep -qE '^conmon version 3(\.|$)'
+version=$(/usr/bin/conmon --version)
+echo "$version"
+grep -qE '^conmon version 3(\.|$)' <<< "$version"
 
 # Run Podman system tests
 bats /usr/share/podman/test/system/
